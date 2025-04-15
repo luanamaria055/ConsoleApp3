834bf11bcd27237faf5c92175ea1cb74f52bda0f
luanamaria055 <luana.mendes051426@gmail.com>
2025-04-14 21:12:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números pares no intervalo [0, 20] em ordem crescente:");
+
+        int numero = 0; // Começamos do 0
+
+        while (numero <= 20) // Enquanto o número for menor ou igual a 20
+        {
+            Console.WriteLine(numero); // Imprime o número par atual
+            numero += 2; // Incrementa para o próximo número par
+        }
+
+        Console.WriteLine("Fim do intervalo.");
+        Console.ReadLine(); // Para manter a janela aberta no Visual Studio
+    }
+}
